5d176172f874fcf31daefbfb663c6d90efae2297
Patr56 <patr56@yandex.ru>
2019-10-24 23:23:50 +0300

    PersonController
    ADD:
    - test update/remove method
    - add travis build

--- src/main/java/pro/horoshilov/family/entity/Photo.java
+++ src/main/java/pro/horoshilov/family/entity/Photo.java
@@ -1,5 +1,7 @@
 package pro.horoshilov.family.entity;
 
+import java.util.Objects;
+
 /**
  * Фотография.
  */
@@ -13,8 +15,24 @@ public class Photo {
 
     private Type type;
 
-    public Photo(String url, Type type) {
+    public Photo(final Long id, final String url, final Type type) {
+        this.id = id;
         this.url = url;
+        this.type = type;
+    }
+
+    public Photo() {
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public void setUrl(final String url) {
+        this.url = url;
+    }
+
+    public void setType(final Type type) {
         this.type = type;
     }
 
@@ -33,5 +51,29 @@ public class Photo {
     public enum Type {
         FRONT,
         BACK
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Photo photo = (Photo) o;
+        return Objects.equals(id, photo.id) &&
+                Objects.equals(url, photo.url) &&
+                type == photo.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, url, type);
+    }
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "id=" + id +
+                ", url='" + url + '\'' +
+                ", type=" + type +
+                '}';
     }
 }
